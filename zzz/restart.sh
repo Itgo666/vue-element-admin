d441c240727eb78b6512f7a658bbf65a79bcb272
Itgo <ilovemou930421@163.com>
2019-01-16 22:00:19 +0800

    restart.sh

---
+++ zzz/restart.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "********************************"
+rmt=yk001
+path="/var/local/h5/web"
+dist="/Users/yangceo/Documents/from_github/vue-element-admin/dist/*"
+
+#npm run build:prod
+
+scp -r $dist "$rmt":"$path"
+echo done!
+echo "********************************"
