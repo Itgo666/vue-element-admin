7985f168d185ae9c76efdaa38b0b51cd37446b04
Itgo <ilovemou930421@163.com>
2019-02-19 12:05:54 +0800

    dev

--- zzz/restart.sh
+++ zzz/restart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "********************************"
 rmt=yk001
-path="/var/local/h5/web"
+path="/var/local/h5/model"
 dist="/Users/yangceo/Documents/from_github/vue-element-admin/dist/*"
 
 #npm run build:prod
